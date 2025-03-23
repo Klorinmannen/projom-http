55ee043418b4f0a29b5c473d8d61bce0426c2f2b
Jonatan Rao-Fransson <jonte.frasse@gmail.com>
2025-03-23 21:24:24 +0100

    Reworked Input and Request classes

--- src/Http/Input.php
+++ src/Http/Input.php
@@ -4,59 +4,21 @@ declare(strict_types=1);
 
 namespace Projom\Http;
 
-use Projom\Util\File;
-
 class Input
 {
-	private readonly array $request;
-	private readonly string $method;
-	private readonly string $payload;
-	private readonly string $url;
-	private readonly array $headers;
+	public array $request;
+	public array $server;
+	public string $payload;
 
 	public function __construct(array $request, array $server, string $payload)
 	{
 		$this->request = $request;
-		$this->headers = $this->parseHeaders($server);
+		$this->server = $server;
 		$this->payload = $payload;
-		$this->method = strtoupper($server['REQUEST_METHOD'] ?? '');
-		$this->url = $server['REQUEST_URI'] ?? '';
 	}
 
 	public static function create(array $request, array $server, string $payload): Input
 	{
 		return new Input($request, $server, $payload);
-	}
-
-	public function parseHeaders(array $server): array
-	{
-		$pattern = '/^HTTP_.*$/';
-		$resultKeys = preg_grep($pattern, array_keys($server));
-		return array_intersect_key($server, array_flip($resultKeys));
-	}
-
-	public function get(string $key, mixed $default = null): mixed
-	{
-		return $this->request[$key] ?? $default;
-	}
-
-	public function payload(): string
-	{
-		return $this->payload;
-	}
-
-	public function method(): string
-	{
-		return $this->method;
-	}
-
-	public function url(): string
-	{
-		return $this->url;
-	}
-
-	public function headers(): array
-	{
-		return $this->headers;
 	}
 }
--- src/Http/Request.php
+++ src/Http/Request.php
@@ -11,12 +11,14 @@ class Request
     protected null|Input $input = null;
     protected string $path = '';
     protected array $parsedUrl = [];
+    protected array $headers = [];
     protected array $queryParameters = [];
 
     public function __construct(Input $input)
     {
         $this->input = $input;
         $this->parseUrl();
+        $this->parseHeaders();
     }
 
     public static function create(null|Input $input = null): Request
@@ -31,7 +33,7 @@ class Request
 
     private function parseUrl(): void
     {
-        $this->parsedUrl = parse_url($this->input->url());
+        $this->parsedUrl = parse_url($this->input->server['REQUEST_URI'] ?? '');
 
         $queryList = $this->parsedUrl['query'] ?? '';
         parse_str($queryList, $this->queryParameters);
@@ -39,25 +41,46 @@ class Request
         $this->path = $this->parsedUrl['path'] ?? '';
     }
 
+	private function parseHeaders(): void
+	{
+        if ($this->input === null)
+            return;
+        
+		$pattern = '/^HTTP_.+$/';
+        $serverKeys = array_keys($this->input->server);
+		$foundHttpKeys = preg_grep($pattern, $serverKeys);
+		$this->headers = array_intersect_key($this->input->server, array_flip($foundHttpKeys));
+	}
+
     public function empty(): bool
     {
         return empty($this->path);
     }
 
-    public function header(string $header): null|string
+    public function headers(null|string $header = null): null|array|string
     {
-        $headers = $this->input->headers();
-        return $headers[$header] ?? null;
+        if ($header === null)
+            return $this->headers;
+
+        return $this->headers[$header] ?? null;
     }
+
+	public function vars(null|string $key = null, mixed $default = null): mixed
+	{
+        if ($key === null)
+            return $this->input->request;
+
+		return $this->input->request[$key] ?? $default;
+	}
 
     public function payload(): string
     {
-        return $this->input->payload();
+        return $this->input->payload;
     }
 
     public function method(): Method
     {
-        return Method::from($this->input->method());
+        return Method::from($this->input->server['REQUEST_METHOD'] ?? '');
     }
 
     public function path(): string

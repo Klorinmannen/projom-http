215065c4229d6eedefc78e58fff10b5e4b4e3095
Jonatan Rao-Fransson <jonte.frasse@gmail.com>
2025-05-21 22:42:26 +0200

    Refactor ContentType class to enum and update content type values with 'Content-Type:' prefix

--- src/Http/ContentType.php
+++ src/Http/ContentType.php
@@ -4,15 +4,15 @@ declare(strict_types=1);
 
 namespace Projom\Http;
 
-class ContentType
+enum ContentType: string
 {
-	const APPLICATION_JSON = 'application/json';
-	const APPLICATION_XML = 'application/xml';
-	const APPLICATION_ZIP = 'application/zip';
-	const APPLICATION_PDF = 'application/pdf';
-	const TEXT_HTML = 'text/html';	
-	const TEXT_PLAIN = 'text/plain';
-	const TEXT_CSS = 'text/css';
-	const TEXT_JAVASCRIPT = 'text/javascript';
-	const TEXT_CSV = 'text/csv';
+	case APPLICATION_JSON = 'Content-Type: application/json';
+	case APPLICATION_XML = 'Content-Type: application/xml';
+	case APPLICATION_ZIP = 'Content-Type: application/zip';
+	case APPLICATION_PDF = 'Content-Type: application/pdf';
+	case TEXT_HTML = 'Content-Type: text/html';
+	case TEXT_PLAIN = 'Content-Type: text/plain';
+	case TEXT_CSS = 'Content-Type: text/css';
+	case TEXT_JAVASCRIPT = 'Content-Type: text/javascript';
+	case TEXT_CSV = 'Content-Type: text/csv';
 }

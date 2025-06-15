c482ab28b37818368652b36179384d9f849a64c7
Jonatan Rao-Fransson <jonte.frasse@gmail.com>
2025-06-15 22:32:13 +0200

    Refactoring

--- src/Http/MiddlewareInterface.php
+++ src/Http/MiddlewareInterface.php
@@ -4,11 +4,7 @@ declare(strict_types=1);
 
 namespace Projom\Http;
 
-use Projom\Http\Request;
-use Projom\Http\Response;
-
 interface MiddlewareInterface
 {
-	public function processBeforeRouting(Request $request): void;
-	public function processAfterRouting(Request $request, Response $response): void;
+	public function process(...$args): void;
 }

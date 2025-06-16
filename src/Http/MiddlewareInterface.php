7b417ff2a2866c43dbfbf2ccc5a17661311e0d87
Jonatan Rao-Fransson <jonte.frasse@gmail.com>
2025-06-16 21:15:57 +0200

    Reinstated typed variabels Request and ?Response as arguments

--- src/Http/MiddlewareInterface.php
+++ src/Http/MiddlewareInterface.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Projom\Http;
 
+use Projom\Http\Request;
+use Projom\Http\Response;
+
 interface MiddlewareInterface
 {
-	public function process(object ...$args): void;
+	public function process(Request $request, Response|null $response = null): void;
 }

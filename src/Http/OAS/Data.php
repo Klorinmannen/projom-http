4636c4c9f1d6dbfe0bd831bd9084f0a878332ed9
Jonatan Rao-Fransson <jonte.frasse@gmail.com>
2025-08-18 10:09:40 +0200

    Correction from previous changes that did not follow through. Renamed private data members for consistency.

--- src/Http/OAS/Data.php
+++ src/Http/OAS/Data.php
@@ -5,12 +5,15 @@ declare(strict_types=1);
 namespace Projom\Http\OAS;
 
 use Projom\Http\Method;
+use Projom\Http\Request;
+use Projom\Http\Response as httpResponse;
+use Projom\Http\OAS\Response;
 
 class Data
 {
-	public array $expectedParameters = [];
-	public array $expectedPayload = [];
-	public array $expectedResponse = [];
+	public array $requiredParameters = [];
+	public array $requiredPayload = [];
+	public array $requiredResponse = [];
 	public bool $security = false;
 	public array $controllerDetails = [];
 	public Method $method;
@@ -21,11 +24,25 @@ class Data
 	{
 		$data = new Data();
 		$data->method = $method;
-		$data->expectedParameters = Parameter::normalize($routeData['parameters'] ?? []);
-		$data->expectedPayload = Payload::normalize($routeData['requestBody'] ?? []);
-		$data->expectedResponse = Response::normalize($routeData['responses'] ?? []);
+		$data->requiredParameters = Parameter::normalize($routeData['parameters'] ?? []);
+		$data->requiredPayload = Payload::normalize($routeData['requestBody'] ?? []);
+		$data->requiredResponse = Response::normalize($routeData['responses'] ?? []);
 		$data->security = Security::normalize($routeData['security'] ?? []);
 		$data->controllerDetails = Controller::normalize($routeData['operationId'] ?? '');
 		return $data;
+	}
+
+	public function verify(Request $request): void
+	{
+		if (!Payload::verify($request->payload(), $this->requiredPayload ?? []))
+			httpResponse::reject('Provided payload does not match expected');
+
+		$normalizedPathParams = Parameter::normalize($this->requiredParameters['path'] ?? []);
+		if (!Parameter::verifyPath($request->pathParameters(), $normalizedPathParams))
+			httpResponse::reject('Provided path parameters do not match expected');
+
+		$normalizedQueryParams = Parameter::normalize($this->requiredParameters['query'] ?? []);
+		if (!Parameter::verifyRequired($request->queryParameters(), $normalizedQueryParams))
+			httpResponse::reject('Provided query parameters do not match expected');
 	}
 }
--- src/Http/OAS/Route.php
+++ src/Http/OAS/Route.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 namespace Projom\Http\OAS;
 
 use Projom\Http\Method;
-use Projom\Http\Request;
-use Projom\Http\Response;
 use Projom\Http\Route\Action;
 use Projom\Http\Route\RouteBase;
 
@@ -22,19 +20,5 @@ class Route extends RouteBase
 		$controller = $this->matchedData->controllerDetails['controller'];
 		$controllerMethod = $this->matchedData->controllerDetails['method'];
 		$this->action = Action::create($controller, $controllerMethod);
-	}
-
-	protected function verifyData(Request $request): void
-	{
-		if (!Payload::verify($request->payload(), $this->matchedData->expectedPayload ?? []))
-			Response::reject('Provided payload does not match expected');
-
-		$normalizedPathParams = Parameter::normalize($this->matchedData->expectedParameters['path'] ?? []);
-		if (!Parameter::verifyPath($request->pathParameters(), $normalizedPathParams))
-			Response::reject('Provided path parameters do not match expected');
-
-		$normalizedQueryParams = Parameter::normalize($this->matchedData->expectedParameters['query'] ?? []);
-		if (!Parameter::verifyExpectedParameters($request->queryParameters(), $normalizedQueryParams))
-			Response::reject('Provided query parameters do not match expected');
 	}
 }
